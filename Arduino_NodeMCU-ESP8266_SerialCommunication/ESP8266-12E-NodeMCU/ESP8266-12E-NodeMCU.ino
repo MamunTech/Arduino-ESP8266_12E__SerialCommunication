ad431aadd650a34befb386c529619a61edd68220
unknown <amamun860@gmail.com>
2020-01-12 21:26:37 +0600

    First commit

---
+++ Arduino_NodeMCU-ESP8266_SerialCommunication/ESP8266-12E-NodeMCU/ESP8266-12E-NodeMCU.ino
@@ -0,0 +1,53 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+
+void setup()
+{
+  
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  
+ 
+  handleIndex();
+
+  delay(3000);
+}
+
+void handleIndex()
+{
+  // Send a JSON-formatted request with key "type" and value "request"
+  // then parse the JSON-formatted response with keys "gas" and "distance"
+  DynamicJsonDocument doc(1024);
+  double gas = 0, distance = 0;
+  // Sending the request
+  doc["type"] = "request";
+  serializeJson(doc,Serial);
+  // Reading the response
+  boolean messageReady = false;
+  String message = "";
+  while(messageReady == false) { // blocking but that's ok
+    if(Serial.available()) {
+      message = Serial.readString();
+      //Serial.println(message);
+      messageReady = true;
+    }
+  }
+  // Attempt to deserialize the JSON-formatted message
+  DeserializationError error = deserializeJson(doc,message);
+  if(error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+  distance = doc["distance"];
+  gas = doc["gas"];
+  Serial.println(distance);
+  Serial.println(gas);
+
+
+}
